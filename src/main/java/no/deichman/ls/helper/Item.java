325c2c5f9707cabb6198ef4ffe9965b44dc5f092
Rurik Greenall <rurik.greenall@gmail.com>
2015-01-16 10:21:35 +0100

    Add helper classes
    Helper classes to define/describe data transformations

---
+++ src/main/java/no/deichman/ls/helper/Item.java
@@ -0,0 +1,50 @@
+package no.deichman.ls.helper;
+
+public class Item {
+    public String withdrawn ;
+    public String biblioitemnumber ;
+    public String restricted ;
+    public String coded_location_qualifier ;
+    public String holdingbranchname ;
+    public String itemlost_on ;
+    public String notforloan ;
+    public String replacementpricedate ;
+    public String itemnumber ;
+    public String ccode ;
+    public String itemnotes ;
+    public String location ;
+    public String itemcallnumber ;
+    public String stack ;
+    public String itemlost ;
+    public String barcode ;
+    public String date_due ;
+    public String uri ;
+    public String datelastseen ;
+    public String materials ;
+    public String price ;
+    public String issues ;
+    public String withdrawn_on ;
+    public String homebranch ;
+    public String replacementprice ;
+    public String more_subfields_xml ;
+    public String cn_source ;
+    public String timestamp ;
+    public String homebranchname ;
+    public String booksellerid ;
+    public String biblionumber ;
+    public String renewals ;
+    public String holdingbranch ;
+    public String damaged ;
+    public String cn_sort ;
+    public String stocknumber ;
+    public String reserves ;
+    public String dateaccessioned ;
+    public String datelastborrowed ;
+    public String enumchron ;
+    public String copynumber ;
+    public String permanent_location ;
+    public String onloan ;
+    public String paidfor ;
+    public String itype ;
+    
+}
---
+++ src/main/java/no/deichman/ls/helper/ItemList.java
@@ -0,0 +1,9 @@
+package no.deichman.ls.helper;
+
+import java.util.HashMap;
+
+public class ItemList {
+	
+	public HashMap<Integer,no.deichman.ls.helper.Item> ItemList = new HashMap<Integer, no.deichman.ls.helper.Item>();
+
+}
