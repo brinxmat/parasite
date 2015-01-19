803942d653196563583ed229cc3d0892e242d25e
Rurik Greenall <rurik.greenall@gmail.com>
2015-01-19 17:54:03 +0100

    Add preliminary test for DataDeichmanAdapterDefault

---
+++ src/test/java/no/deichman/ls/consumer/DataDeichmanAdapterDefaultTest.java
@@ -0,0 +1,27 @@
+package no.deichman.ls.consumer;
+
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+
+import no.deichman.ls.adapter.DataDeichmanAdapter;
+import no.deichman.ls.adapter.DataDeichmanAdapterDefault;
+
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+import org.junit.Test;
+
+import com.hp.hpl.jena.rdf.model.Model;
+
+public class DataDeichmanAdapterDefaultTest {
+
+	@Test
+	public void test() throws FileNotFoundException {
+		DataDeichmanAdapterDefault x = new DataDeichmanAdapterDefault();
+		Model m = x.describeWork("749919");
+        RDFDataMgr.write(System.out, m, Lang.TRIG);
+
+
+	}
+
+}
